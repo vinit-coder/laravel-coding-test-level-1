0af75a3cb65c9a6c4a9c5281d1914f28c0b51361
rohitadreno <rohit@webworldexpertsindia.com>
2022-05-19 13:00:19 +0530

    users

--- demo/database/seeds/DatabaseSeeder.php
+++ demo/database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $users = factory(App\User::class, 5)->make();
+        $users = factory(App\User::class, 5)->create();
         $events = factory(App\Event::class, 10)->create();
     }
 }
